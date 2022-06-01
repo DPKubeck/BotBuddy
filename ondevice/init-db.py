14389d0fad0164a900818856d22c7c0f45f3958f
Cozmo8 <97196858+Cozmo8@users.noreply.github.com>
2022-06-01 15:59:36 -0700

    Create init-db.py

---
+++ ondevice/init-db.py
@@ -0,0 +1,26 @@
+import mysql.connector as mysql
+import os
+from dotenv import load_dotenv #only required if using dotenv for creds
+ 
+load_dotenv('credentials.env')
+
+db_host = os.environ['MYSQL_HOST']
+db_user = os.environ['MYSQL_USER']
+db_pass = os.environ['MYSQL_PASSWORD']
+db_name = os.environ['MYSQL_DATABASE']
+
+db = mysql.connect(user=db_user, password=db_pass, host=db_host, database=db_name)
+cursor = db.cursor()
+cursor.execute("DROP TABLE IF EXISTS Speech_Input;")
+ 
+try:
+  cursor.execute("""
+    CREATE TABLE Speech_Input (
+      id          integer  AUTO_INCREMENT PRIMARY KEY,
+      text        VARCHAR(50) NOT NULL,  
+      time        TIMESTAMP
+    );
+  """)
+
+except RuntimeError as err:
+  print("runtime error: {0}".format(err))
